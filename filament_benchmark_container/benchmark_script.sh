458ac59af769e1a39dd07f22e49e5fdb9bf03b41
Devin Schwab <digidevin@gmail.com>
2014-11-30 01:08:31 -0500

    Fixed some bugs in the dockerfiles and the scripts

--- filament_benchmark_container/benchmark_script.sh
+++ filament_benchmark_container/benchmark_script.sh
@@ -2,7 +2,10 @@
 
 set -e 
 
-java -jar /filament_benchmark_jar/filament_benchmark.jar $FILAMENT_POSTGRES_PORT_5432_TCP_ADDR $FILAMENT_POSTGRES_PORT_5432_TCP_PORT filament /raw_data.nt $NUM_EDGES > /output/benchmark_outputs_$NUM_EDGES.txt 2>&1
+echo $FILAMENT_POSTGRES_PORT_5432_TCP_ADDR
+echo $FILAMENT_POSTGRES_PORT_5432_TCP_PORT
+echo $NUM_EDGES
 
-psql -h $FILAMENT_POSTGRES_PORT_5432_TCP_ADDR $FILAMENT_POSTGRES_PORT_5432_TCP_PORT filament filament -c "select t1.datname AS db_name,  pg_size_p│Devins-MacBook-Pro:eecs_433_project rhol$ touch ~/boot2docker/benchmark_data.txt
-retty(pg_database_size(t1.datname)) as db_size from pg_database as t1 WHERE t1.datname = 'filament';" > /output/db_size_$NUM_EDGES.txt 2>&1
+echo "Starting benchmarks"
+java -jar /filament_benchmark_jar/filament_benchmark.jar $FILAMENT_POSTGRES_PORT_5432_TCP_ADDR $FILAMENT_POSTGRES_PORT_5432_TCP_PORT filament /raw_data.nt $NUM_EDGES > /output/benchmark_outputs_$NUM_EDGES.txt 2>&1
+echo "Finished benchmarks"

12bf59f2da257e404f69c278b93f38368554c36b
Devin Schwab <digidevin@gmail.com>
2014-11-30 03:21:57 -0500

    Updated the neo4j and dex containers with new files from Yigit

--- neo4j_benchmark_container/benchmark_script.sh
+++ neo4j_benchmark_container/benchmark_script.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "Starting benchmarks"
-java -jar /neo4j.jar $NUM_EDGES > /output/neo4j_benchmark_outputs_$NUM_EDGES.txt 2>&1
+java -Xmx1024m -jar /neo4j.jar $NUM_EDGES > /output/neo4j_benchmark_outputs_$NUM_EDGES.txt 2>&1
 ls -lh > /output/neo4j_db_size_$NUM_EDGES.txt 2>&1
 echo "Finished benchmarks"
